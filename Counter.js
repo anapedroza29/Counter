2ceb62bb33d0b33a53d52b2ba250da8a2ad65834
anapedroza29 <100169738+anapedroza29@users.noreply.github.com>
2022-03-21 14:43:04 -0700

    Counter
    add myfuntion and myfuntion1

--- Counter.js
+++ Counter.js
@@ -1,8 +1,41 @@
 
 // document.querySelectorAll()
-const matches = document.querySelectorAll("favorite styled");
-// forEach()
-// addEventListener()
-// currentTarget property
-// classList
-// textContent
+let count = 0;
+const value = document.querySelector('#value');
+//console.log(value);
+const btns = document.querySelectorAll(".btn");
+//console.log(btns);
+   
+ // forEach btns create a eventlistener for our class list. when I clik the btns it should decrease, increase or reset. 
+
+btns.forEach(myfuntion)
+function myfuntion (btn) {
+  console.log (btn)
+btn.addEventListener('click', myfuntion1)
+function myfuntion1 (list) {
+  console.log (list)
+const styles = list.currentTarget.classList;
+    if (styles.contains('decrease')){
+    count --;
+    } else if (styles.contains('increase')){
+    count ++;
+    } else {
+      count = 0;
+    }
+   
+    if (count > 0){
+      value.style.color = ("red")
+      console.log(value)
+    } if (count < 0){
+      value.style.color = ("blue")
+    } if (count === 0){
+      value.style.color = ("black")
+
+    }
+
+    value.textContent = count;
+
+}
+};
+
+
